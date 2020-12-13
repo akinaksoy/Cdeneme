f6cdf612cee125ac517dd5c74c574585cb7a51c0
Akın Aksoy <akinaksoytr@gmail.com>
2020-12-13 16:51:44 +0100

    Vıze fınal hesaplama eklendi git add .

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char** argv) {
-	
+	//Toplama
 	int a,b ; 
 	printf("Bir sayi giriniz : ");
 	scanf("%d",&a);
@@ -11,6 +11,21 @@ int main(int argc, char** argv) {
 	scanf("%d",&b);
 	int c = a+b;
 	printf("iki sayinin toplami = %d",c);
+	
+	
+	//Vize final hesaplama
+	
+	int vize,final,ortalama;
+	
+	printf("Vize notunuzu giriniz : ");
+	scanf("%d",&vize);
+	printf("Final notunuzu giriniz : ");
+	scanf("%d",&final);
+	
+	ortalama = (vize*0.3) + (final*0.7);
+	
+	printf("Donem ortalamasi = %d",ortalama);
+	
 	
 	
 	
